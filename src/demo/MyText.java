ec3527e4c5c22587a45d8ab60dfca7330e5bca12
John <john.kraczek@gmail.com>
2014-03-05 17:50:58 -0700

    Setting Text

--- src/demo/MyText.java
+++ src/demo/MyText.java
@@ -1,12 +1,23 @@
 package demo;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MyText extends JPanel {
 	private static final long serialVersionUID = 1L;
+	
+	public MyText(){
+	JLabel label = new JLabel("Hello World");
+	label.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+	this.add(label);
+	label.setText("YEAH");
+	this.add(label);
+	}
+	
 
 	public static void main(String[] args){
 		EventQueue.invokeLater(new Runnable(){
